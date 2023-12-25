2641bbf0cfc5ca632ba62285cd3d40203af6c0bd
salivo <andrijsalivon@gmail.com>
2023-12-25 19:59:36 +0000

    emm some commit to not lose it

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, request, url_for
 
 app = Flask(__name__)
+
+app.secret_key = b'_5#y2L"F4rjojoejo%^&*@(*U@)UU@VF&@*9hg9ujhb8287TF@&FG*&@g9&^@g2yg8727286@%^%#$!&*Q8z\n\xec]/'
+
+
 @app.route('/')
 def index():
     return render_template('user.html')
@@ -8,5 +12,17 @@ def index():
 @app.route('/admin/<page>')
 def admin(page):
     return render_template('adminpanel.html', page=page)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('admin',page="Dashboard"))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
 
 app.run(host='0.0.0.0')
--- src/static/scripts/adminpanel.js
+++ src/static/scripts/adminpanel.js
@@ -1,11 +1,42 @@
 
 
 const header_elements = [
-    {id:1, name:"Dashboard"},
-    {id:2, name:"Settings"},
-    {id:3, name:"Profile"},
-    
-]
+    {id:"header_1", name:"Dashboard"},
+    {id:"header_1", name:"Settings"},
+];
+
+
+function ProfileCard(){
+    return (
+        <div className="CardCont">
+            <h2>Public profile</h2>
+            <form>
+                <label>Name</label>
+                <input type="text" id="new_nickname" name="nickname"></input>
+                <label className="description_label">Your name may appear around GitHub where you contribute or are mentioned. You can remove it at any time. </label>
+                <button type="submit" value="Submit">Update Profile</button>
+            </form>
+        </div>
+    );
+}
+
+function AccountCard(){
+    return (
+        <div className="CardCont">
+            <h2>Change password</h2>
+            <form>
+                <label>New password</label>
+                <input type="text" id="new_nickname" name="nickname"></input>
+
+                <label>Confirm new password</label>
+                <input type="text" id="new_nickname" name="nickname"></input>
+                <label className="description_label">Your name may appear around GitHub where you contribute or are mentioned. You can remove it at any time. </label>
+                <button type="submit" value="Submit">Change password</button>
+            </form>
+        </div>
+    );
+}
+
 
 function Header(){
     const elements = header_elements.map((element) =>
@@ -18,26 +49,21 @@ function Header(){
 
 
 function ShowPage(){
-    if (page == "Settings"){
-        return (
-        <div className="Settings_page">
-            <h1>Settings</h1>
-        </div>
-        );
-    }
-    if (page == "Dashboard"){
-        return (
-        <div className="Settings_page">
-            <h1>Dashboard</h1>
-        </div>
-        );
-    }
-    if (page == "Profile"){
-        return (
-        <div className="Settings_page">
-            <h1>Profile</h1>
-        </div>
-        );
+    switch (page) {
+        case "Dashboard":
+            return (
+                <div className="page">
+                    <h1>Dashboard</h1>
+                </div>
+            );
+        case "Settings":
+            return (
+                <div className="page">
+                    <ProfileCard />
+                    <AccountCard />
+                </div>
+            );
+        
     }
 }
 
